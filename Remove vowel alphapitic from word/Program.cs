36c50162faa9252992ba7cc49aec8a92c5c57b71
Youssefat21 <yousefyousef4339@gmail.com>
2022-10-07 10:57:56 +0200

    Remove the vowel letters

---
+++ Remove vowel alphapitic from word/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Remove_vowel_alphapitic_from_word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your word : ");
+            string[] word = new string[1];                    // To available turn it to char array
+            word[0] = Console.ReadLine();
+            
+           char[]  X = word[0].ToCharArray();           // Turn string array to char array
+
+            for (int i=0; i < X.Length; i++)                   // X.Length: is a number of characters 
+            {
+                if (X[i] == 'a' || X[i] == 'u' || X[i] == 'e' || X[i] == 'i' || X[i] == 'o') // exclude the alphapitic vowels
+                {
+                    X[i] = '_';
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------");
+ 
+            for (int j =0; j< X.Length; j++)                 // Display the result
+            {
+                Console.Write(X[j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
